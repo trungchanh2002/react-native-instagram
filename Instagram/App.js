65d537c65e0020b6a8acfcd9c21b30db53e70f10
trungchanh2002 <trungchanh.kda@gmail.com>
2023-11-25 02:23:03 +0700

    Update App.js

--- Instagram/App.js
+++ Instagram/App.js
@@ -14,6 +14,7 @@ import StoryScreen from "./components/StoryScreen";
 import UpdateScreen from "./components/UpdateScreen";
 import CommentScreen from "./components/CommentScreen";
 
+//Test
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
