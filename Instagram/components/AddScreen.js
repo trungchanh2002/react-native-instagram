fc0f8983ec8d1645ce4dfb04b62e3db78cf79709
trungchanh2002 <trungchanh.kda@gmail.com>
2023-11-26 03:20:14 +0700

    Update HomeScreen

--- Instagram/components/AddScreen.js
+++ Instagram/components/AddScreen.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function AddScreen() {
   return (
     <View style={styles.container}>
-      <Text>AddScreen</Text>
+      <TouchableOpacity>
+        <Text>AddScreen</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,8 @@ export default function AddScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
